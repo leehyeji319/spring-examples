2ca5aeb45c4b107ade803cc5204a1f802dbba4b0
Hyeji Lee <zzambbang@gmail.com>
2022-01-21 17:41:20 +0900

    feat: annotation 생성해보고 모든 빈 조회해보기

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,9 +1,12 @@
 package hello.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-
+@Component //rate fix 둘다 빈으로 등록해보기
 public class FixDiscountPolicy implements DiscountPolicy{
 
 	private int discountFixAmount = 1000; //1000원 할인
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,8 +1,10 @@
 package hello.core.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -12,13 +14,19 @@ import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor //final이 붙으면 필수값이 붙잖아? final이 붙은거를 생성자를 만들어준다.
+//@RequiredArgsConstructor //final이 붙으면 필수값이 붙잖아? final이 붙은거를 생성자를 만들어준다.
 public class OrderServiceImpl implements OrderService{
 
 	//final이 되어잇으면 기본이든 생성자를 통해서 할당이 되어야함
 //	@Autowired //생성자 위에 오토와이어드
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy; //이렇게 하면 인터페이스에만 의존하는거니까 DIP 해결 ? ㅋㅋ 근데 구현은 없잔아 ... ;;
+
+	@Autowired
+	public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {//단일 책임 원칙 짱 잘 설계
