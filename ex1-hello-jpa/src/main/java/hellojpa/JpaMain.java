3abe5b525c9bb64da164783df30777504d316176
Hyeji Lee <zzambbang@gmail.com>
2022-06-10 14:48:58 +0900

    feat: 영속성 컨텍스트까지

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,12 +19,23 @@ public class JpaMain {
 
 		try {
 			//비영속상태
-			Member member = new Member();
-			member.setId(100L);
-			member.setName("HelloJPA");
+			// Member member = new Member();
+			// member.setId(101L);
+			// member.setName("HelloJPA");
 
 			//영속
-			em.persist(member); //이 때 DB에 저장되지 않음
+			// System.out.println("=== BEFORE ===");
+			// em.persist(member); //이 때 DB에 저장되지 않음
+			// System.out.println("=== AFTER ===");
+
+			Member findMember = em.find(Member.class, 101L);
+			Member findMember2 = em.find(Member.class, 101L);
+
+			System.out.println("result = " + (findMember == findMember2));
+
+			// System.out.println("findMember.id = " + findMember.getId());
+			// System.out.println("findMember.name = " + findMember.getName());
+			//select query가 나가지 않음
 
 			tx.commit(); //커밋을 꼭해줘야함 안하면 반영이 안돼
 		} catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -15,6 +15,14 @@ public class Member {
 	//@Column(name = "username")
 	private String name;
 
+	public Member() { //jpa는 기본적으로 내부적으로 reflection같은걸 써서, 동적으로 객체를 생성해내야한다. 기본생성자잇어야함
+	}
+
+	public Member(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 	public Long getId() {
 		return id;
 	}
